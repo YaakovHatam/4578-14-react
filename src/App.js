22dcba223b1f60ef648c404498f04b4cab973c8d
YaakovHatam <kobieshka@gmail.com>
2021-01-24 21:41:32 +0200

    starring works

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import './App.css';
 import StudentProfile from './components/student-profile';
 import { Segment } from 'semantic-ui-react'
+import { useState } from 'react';
 
 function App() {
-    const usersArray = ['Yohan-jb26', 'saifsandouka', 'yonimendel', 'YaakovHatam', 'alexandreguz', 'Egegas', 'ArielgGodlevsky'];
+    const usersArray = ['yohan-jb26', 'saifsandouka', 'yonimendel', 'YaakovHatam', 'alexandreguz', 'Egegas', 'ArielgGodlevsky'];
+    const [bookmarkedUsers, setBookmarkedUsers] = useState(['yohan-jb26']);
+
+    const toggleBookmarked = (username) => {
+        const userIndex = bookmarkedUsers.indexOf(username);
+        if (userIndex > -1) {
+            debugger;
+            bookmarkedUsers.splice(userIndex, 1)
+            setBookmarkedUsers([...bookmarkedUsers]);
+        } else {
+            setBookmarkedUsers([...bookmarkedUsers, username]);
+        }
+
+        console.log(bookmarkedUsers);
+    }
     return (
         <Segment.Group horizontal>
-            {usersArray.map(u => <Segment key={u}><StudentProfile username={u} /></Segment>)}
+            {usersArray.map(u => <Segment key={u}><StudentProfile
+                bookmarked={bookmarkedUsers.indexOf(u) > -1}
+                onBookmarkUpdate={toggleBookmarked}
+                username={u} />
+            </Segment>)}
         </Segment.Group>
     );
 }
--- src/components/student-profile.js
+++ src/components/student-profile.js
@@ -1,10 +1,46 @@
 import React, { useState, useEffect } from 'react';
-import { Card, Icon, Image } from 'semantic-ui-react';
+import { Card, Icon, Image, Button } from 'semantic-ui-react';
 
-const fetchProfile = (username) => fetch(`https://api.github.com/users/${username}`).then(r => r.json());
+// const fetchProfile = (username) => fetch(`https://api.github.com/users/${username}`).then(r => r.json());
+const fetchProfile = (username) => new Promise((res, rej) => {
+    res({
+        "login": username,
+        "id": 6986221,
+        "node_id": "MDQ6VXNlcjY5ODYyMjE=",
+        "avatar_url": "https://avatars.githubusercontent.com/u/6986221?v=4",
+        "gravatar_id": "",
+        "url": "https://api.github.com/users/YaakovHatam",
+        "html_url": "https://github.com/YaakovHatam",
+        "followers_url": "https://api.github.com/users/YaakovHatam/followers",
+        "following_url": "https://api.github.com/users/YaakovHatam/following{/other_user}",
+        "gists_url": "https://api.github.com/users/YaakovHatam/gists{/gist_id}",
+        "starred_url": "https://api.github.com/users/YaakovHatam/starred{/owner}{/repo}",
+        "subscriptions_url": "https://api.github.com/users/YaakovHatam/subscriptions",
+        "organizations_url": "https://api.github.com/users/YaakovHatam/orgs",
+        "repos_url": "https://api.github.com/users/YaakovHatam/repos",
+        "events_url": "https://api.github.com/users/YaakovHatam/events{/privacy}",
+        "received_events_url": "https://api.github.com/users/YaakovHatam/received_events",
+        "type": "User",
+        "site_admin": false,
+        "name": "yaakov hatam",
+        "company": "@codestackacademy ",
+        "blog": "",
+        "location": "Jerusalem, Israel",
+        "email": null,
+        "hireable": true,
+        "bio": null,
+        "twitter_username": null,
+        "public_repos": 54,
+        "public_gists": 13,
+        "followers": 66,
+        "following": 7,
+        "created_at": "2014-03-18T11:04:24Z",
+        "updated_at": "2021-01-24T15:47:25Z"
+    });
+});
 
 function StudentProfile(props) {
-    const { username } = props;
+    const { username, bookmarked } = props;
     const [userData, setUserData] = useState({});
 
     useEffect(() => {
@@ -24,7 +60,9 @@ function StudentProfile(props) {
                 </Card.Description>
             </Card.Content>
             <Card.Content extra>
-                <a><Icon name='user' />{userData.followers} Friends</a>
+                <Button basic color='blue' onClick={(e) => props.onBookmarkUpdate(username)}>
+                    <Icon name={'star' + (!bookmarked ? ' outline' : '')} /> Bookmark
+                </Button>
             </Card.Content>
         </Card> : <></>
     );
